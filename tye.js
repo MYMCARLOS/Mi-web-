03cbd69bd48d78280d409dda0167c59587b36575
charly000759@gmail.com <charly000759@gmail.com>
2022-11-23 13:33:50 -0600

    arreglo de h1

---
+++ tye.js
@@ -0,0 +1,69 @@
+// saludar();
+
+// var numero=19;
+// console.log(numero);
+// console.log("Mi edad es: " + numero)
+
+// const miConstante = 5;
+// console.log("El valor de mi constante es: " + miConstante);
+
+// var resultado = 3 + 3;
+// console.log ("el valor de la suma es: "+resultado)
+
+// // CONDICIONALES 
+// let miNumero12 = 6;
+// let miNombre ="Carlos";
+// // let type1 = miNumero12 == 6;
+// // console.log(type1);
+
+// if (miNombre=== "Carlos" && miNumero12 !=6) {
+//     console.log("Si ")
+    
+// }else{
+//     console.log("No ")
+// }
+
+// let contador = 0 ;
+// while (contador <=5) {
+//     console.log(contador)
+//     contador=contador + 1;
+    
+// }
+// // for (let i=1000; i>=0; i--){
+// //     console.log(i);
+// // }
+// function saludar(nombre,edad){
+//     console.log("Hola, mi nombre es: " +nombre)
+//     console.log("Mi edad es: " +edad )
+// }
+// function multiplicador(num1,num2){
+//     let resultado= num1+num2
+//     return resultado
+// }
+// console.log(multiplicador(5,5));
+let MiArreglo= ["Carlos", "Fer ", "Kevin ", "Carla", "Carlos1", "Fer1 ", "Kevin1 ", "Carla1"];
+
+// for (let i=0; i <10; i++) {
+// console.log ("Accediendo Al Indice: "+ i);
+// let mostrar = MiArreglo[i];
+// console.log (mostrar)
+// }
+// Objetos 
+
+let Persona1 = {
+    nombre : "Carlos",
+    edad: 24, 
+    masculino: true
+};
+let Persona2 ={
+    nombre : "Fer",
+    edad: 21, 
+    masculino: false,
+
+}
+console.log(Persona1);
+ 
+Persona1.comidaFavorita = "Hamburgesa"; 
+
+
+console.log(Persona2);
